d32026ae0808de0f36b3d18d95b2dbd00b26a057
TimWeiHu <win94717@gmail.com>
2019-02-10 00:42:48 +0800

    final

--- password-retry.py
+++ password-retry.py
@@ -1,12 +1,16 @@
 password = 'a123456' #設定密碼
 i = 3
 while i > 0:
+    i = i - 1
     x = input('請輸入密碼:')
     if x == password:
         print('登入成功！')
         break
     else:
-        i = i - 1
-        print('密碼錯誤！你還有', i, '次機會')
+        print('密碼錯誤！')
+        if i > 0:
+            print('你還有', i, '次機會')
+        else:
+            print('沒機會囉！')
 
 
